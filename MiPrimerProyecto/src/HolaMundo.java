2205b2c70c3ad1645b23e654867ae9be5d4e849c
dcfernandez <dcfernandez@indra.es>
2022-07-26 11:35:53 +0200

    Versión 1.1 de HolaMundo

--- MiPrimerProyecto/src/HolaMundo.java
+++ MiPrimerProyecto/src/HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!");
+		System.out.println("Hola mundo otra vez!");
+		System.out.println("Esta es una actualización del programa");
 	}
 }
